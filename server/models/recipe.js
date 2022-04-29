79c629b6b1e31cc42daa75f4fadfab0e99953a9f
samueljsong <semuall0108@gmail.com>
2022-04-29 15:46:10 -0700

    SCHEMA basic outline
    made the bare bones of other SCHEMA and the validations/constraints

---
+++ server/models/recipe.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const recipeSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 0,
+    maxlength: 20
+  },
+  ingredients: {
+    type: String,
+    required: true,
+  },
+  instructions: {
+    type: String,
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5
+  }
+});
+
+const Recipe = model("Recipe", recipeSchema);
+
+module.exports = Recipe;
