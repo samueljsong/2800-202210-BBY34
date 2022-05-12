241cca62da24bcd4c8a0d77ec190cb7cd40eecf2
Naomi Byun <naomibyun7@gmail.com>
2022-05-12 01:10:49 -0700

    run dotenv only in development environment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 "use strict";
-
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 require("./db/mongoose");
 const bodyParser = require("body-parser");
 const express = require("express");
