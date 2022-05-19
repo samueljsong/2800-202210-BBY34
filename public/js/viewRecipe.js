988ae427062103577c3fd5018fb95baa4c9039bb
samueljsong <semuall0108@gmail.com>
2022-05-19 09:39:28 -0700

    adding basic js files for recipe and view recipe pages

---
+++ public/js/viewRecipe.js
@@ -0,0 +1,11 @@
+const img = document.getElementById('image-box');
+const title = document.getElementById('title');
+const info = document.getElementById('description');
+
+let header = "<h1>";
+header += "MONGO DB";
+header += "</h1>";
+
+let description = "<p>";
+description += "MONGO DB";
+description += "</p>";
