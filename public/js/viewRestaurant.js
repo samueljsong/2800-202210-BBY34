c44e5400a95e3e174c8448b51ce89ff5fea38b2d
Dinuja <97323936+dinujagunawardana@users.noreply.github.com>
2022-05-25 14:37:38 -0700

    Restaurant view JS

--- public/js/viewRestaurant.js
+++ public/js/viewRestaurant.js
@@ -1,37 +1,54 @@
 "use strict";
 //HTTPS request
 ready(function() {
-  function ajaxGET(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-      if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        callback(this.responseText);
-      } else {
-        console.log(this.status);
-      }
-    };
-    xhr.open("GET", url);
-    xhr.send();
-  }
+    function ajaxGET(url, callback) {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function() {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                callback(this.responseText);
+            } else {
+                console.log(this.status);
+            }
+        };
+        xhr.open("GET", url);
+        xhr.send();
+    }
 
-  document.querySelector('.info').addEventListener('click', function() {
-    window.location.replace("/recipe");
-  });
+    document.querySelector('.info').addEventListener('click', function() {
+        window.location.replace("/recipe");
+    });
 
-  document.querySelector('.image-box').addEventListener('click', function() {
-    window.location.replace("/recipe");
-  })
+    document.querySelector('.image-box').addEventListener('click', function() {
+        window.location.replace("/recipe");
+    })
 
-  document.getElementById('search-btn').addEventListener('click', function() {
-    window.location.replace('/mainPageUser');
-  })
+    document.getElementById('search-btn').addEventListener('click', function() {
+        window.location.replace('/mainPageUser');
+    })
+
+    window.addEventListener("load", function(e) {
+        e.preventDefault();
+        let currentUser = String(this.localStorage.getItem("currentUserID"));
+        console.log(currentUser);
+
+    });
+
+    window.addEventListener("load", function(e) {
+        e.preventDefault();
+        ajaxGET("/api/restaurant", function(data) {
+            console.log(JSON.parse(data));
+            let dataParsed = JSON.parse(data);
+            console.log(dataParsed);
+        });
+    });
+
 
 });
 
 function ready(callback) {
-  if (document.readyState != "loading") {
-    callback();
-  } else {
-    document.addEventListener("DOMContentLoaded", callback);
-  }
+    if (document.readyState != "loading") {
+        callback();
+    } else {
+        document.addEventListener("DOMContentLoaded", callback);
+    }
 }
