4db9d64fb184cb9cc035817f958247eefe1872e3
Dinuja <97323936+dinujagunawardana@users.noreply.github.com>
2022-05-25 17:10:19 -0700

    JS changes for redirection mainpage

--- public/js/mainPageUser.js
+++ public/js/mainPageUser.js
@@ -1,32 +1,34 @@
 "use strict";
 //HTTPS request
 ready(function() {
-  function ajaxGET(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-      if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        callback(this.responseText);
-      } else {
-        console.log(this.status);
-      }
-    };
-    xhr.open("GET", url);
-    xhr.send();
-  }
+    function ajaxGET(url, callback) {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function() {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                callback(this.responseText);
+            } else {
+                console.log(this.status);
+            }
+        };
+        xhr.open("GET", url);
+        xhr.send();
+    }
 
+    document.getElementById('e').addEventListener('click', function() {
+        window.location.replace("/viewRestNew");
+    })
 
-  document.getElementById('e').addEventListener('click', function() {
-    window.location.replace("/viewRestNew");
-  })
-
+    document.getElementById('rec').addEventListener('click', function() {
+        window.location.replace("/recipe");
+    })
 
 
 });
 
 function ready(callback) {
-  if (document.readyState != "loading") {
-    callback();
-  } else {
-    document.addEventListener("DOMContentLoaded", callback);
-  }
+    if (document.readyState != "loading") {
+        callback();
+    } else {
+        document.addEventListener("DOMContentLoaded", callback);
+    }
 }
